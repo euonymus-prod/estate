d73618eac78627c69a24445c596fde158e325d18
euonymus <euonymus0220@gmail.com>
2021-02-17 23:09:00 +0900

    refactoring

--- web-react/src/__tests__/App.test.js
+++ web-react/src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from '../App'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
