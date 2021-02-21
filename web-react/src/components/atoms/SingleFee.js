11315912661c0a95b48c4bbfa56e71bee080dcde
euonymus <euonymus0220@gmail.com>
2021-02-22 02:40:51 +0900

    chore

---
+++ web-react/src/components/atoms/SingleFee.js
@@ -0,0 +1,23 @@
+const TAX_RATE = 0.1
+export default function SingleFee({
+  decrared_rental_fee,
+  management_fee,
+  common_service_fee,
+  amount,
+  feeObject,
+}) {
+  let tmp = 0
+  if (feeObject.payment_unit == 'relative') {
+    tmp = decrared_rental_fee * amount
+  } else if (feeObject.payment_unit == 'relative_to_full') {
+    tmp = (decrared_rental_fee + management_fee + common_service_fee) * amount
+  } else if (feeObject.payment_unit == 'exact') {
+    tmp = amount
+  } else {
+    return false
+  }
+  if (feeObject.tax_included) {
+    tmp = tmp * (1 + TAX_RATE)
+  }
+  return Math.floor(tmp, 0)
+}
--- web-react/src/components/atoms/YearlyFee.js
+++ web-react/src/components/atoms/YearlyFee.js
@@ -1,29 +1,17 @@
-const TAX_RATE = 0.1
+import SingleFee from './SingleFee'
+
 export default function YearlyFee({ contract }) {
   const feeType = 'yearly_fees'
   const feeObject = 'YearlyFee'
   let ret = 0
   contract[feeType].forEach((feeRelation) => {
-    let tmp = 0
-    if (feeRelation[feeObject].payment_unit == 'relative') {
-      tmp = contract.decrared_rental_fee * feeRelation.amount
-    } else if (feeRelation[feeObject].payment_unit == 'relative_to_full') {
-      tmp =
-        (contract.decrared_rental_fee +
-          contract.management_fee +
-          contract.common_service_fee) *
-        feeRelation.amount
-    } else if (feeRelation[feeObject].payment_unit == 'exact') {
-      tmp = feeRelation.amount
-    } else {
-      return false
-    }
-    if (feeRelation[feeObject].tax_included) {
-      tmp = tmp * (1 + TAX_RATE)
-    }
-    tmp = Math.floor(tmp, 0)
-
-    ret += tmp
+    ret += SingleFee({
+      decrared_rental_fee: contract.decrared_rental_fee,
+      management_fee: contract.management_fee,
+      common_service_fee: contract.common_service_fee,
+      amount: feeRelation.amount,
+      feeObject: feeRelation[feeObject],
+    })
   })
   return ret.toLocaleString()
 }
