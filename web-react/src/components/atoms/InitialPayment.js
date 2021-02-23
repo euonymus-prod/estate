e53345e6d644656096aec3cfe7d2823ac241dcc0
euonymus <euonymus0220@gmail.com>
2021-02-23 13:51:24 +0900

    chore

--- web-react/src/components/atoms/InitialPayment.js
+++ web-react/src/components/atoms/InitialPayment.js
@@ -6,7 +6,7 @@ export default function InitialPayment({ contract, forPrint }) {
   const initialFee = InitialFee({ contract })
   const monthlyFee = MonthlyFee({ contract })
   // TODO: free rent
-  const freeRent = 0
+  // const freeRent = 0
   let ret = initialFee + monthlyFee
 
   if (forPrint) {
